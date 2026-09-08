1e95e5a0e7d4bf4be1a48823b4ecf4bc9d0407ac
lucascorreaa <lucas.lca14@gmail.com>
2026-09-08 11:14:30 -0300

    feat(alert-banner): add switch icons

--- src/components/alert-banner/index.jsx
+++ src/components/alert-banner/index.jsx
@@ -1,8 +1,34 @@
 import { Container, Description } from "./style"
+import { FontAwesome6 } from "@expo/vector-icons";
 
-const AlertBanner = ({ type, message}) => {
+const AlertBanner = ({ type, message }) => {
+
+    function IconType() {
+        switch (type) {
+            case "alert":
+                return "triangle-exclamation"
+                break;
+            case "success":
+                return "triangle-exclamation"
+                break;
+            case "danger":
+                return "triangle-exclamation"
+                break;
+        
+            default:
+                return "triangle-exclamation"
+                break;
+        }
+    }
+    console.log(IconType())
+
     return (
         <Container>
+            <FontAwesome6 
+                name={IconType()}
+                size={24}
+                color="black"
+            />
             <Description>{message}</Description>
         </Container>
     )
